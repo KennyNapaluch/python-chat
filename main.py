bfe451ba105662bdb2a3f20aa0a014968480df85
Kenny Napaluch <121361808+KennyNapaluch@users.noreply.github.com>
2023-09-28 12:28:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import socket
 import threading
 
-def handle_client(client_socket, client_address):
+def receive_messages(client_socket, client_address):
     print(f"Connected to {client_address[0]}:{client_address[1]}")
-
-    try:
-        while True:
+    while True:
+        try:
             message = client_socket.recv(1024).decode('utf-8')
             if not message:
                 break
             print(f"{client_address[0]}:{client_address[1]} says: {message}")
+        except Exception as e:
+            print(f"Error receiving message from {client_address[0]}:{client_address[1]}: {str(e)}")
+            break
 
-    except Exception as e:
-        print(f"Error receiving message from {client_address[0]}:{client_address[1]}: {str(e)}")
+def send_messages(client_socket, client_address):
+    while True:
+        try:
+            message = client_address.recv(1024).decode('utf-8')
+            if not message:
+                break
+            client_socket.send(message.encode('utf-8'))
+        except Exception as e:
+            print(f"Error sending message to {client_address[0]}:{client_address[1]}: {str(e)}")
+            break
 
-    finally:
-        print(f"Connection to {client_address[0]}:{client_address[1]} closed.")
-        client_socket.close()
 
 def main():
     host = '0.0.0.0'
@@ -31,8 +38,12 @@ def main():
 
         while True:
             client_socket, client_address = server_socket.accept()
-            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread = threading.Thread(target=receive_messages, args=(client_socket, client_address))
             client_thread.start()
+
+            # Create a send thread for each client
+            send_thread = threading.Thread(target=send_messages, args=(client_socket, client_socket))
+            send_thread.start()
 
     except Exception as e:
         print(f"Error: {str(e)}")
