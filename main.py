e427a1800368134929714c18814725b6ba6288a9
Kenny Napaluch <121361808+KennyNapaluch@users.noreply.github.com>
2023-09-28 12:46:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,28 +24,50 @@ def send_messages(client_socket, client_address):
             client_socket.close()
             break
 
-def main():
-    host = '0.0.0.0'
-    port = 8888
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+def connect_to_server(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server_socket.bind((host, port))
-        server_socket.listen(5)
-        print("Waiting for connections...")
-
-        while True:
-            client_socket, client_address = server_socket.accept()
-            client_thread = threading.Thread(target=receive_messages, args=(client_socket, client_address))
-            client_thread.start()
-
-            # Create a send thread for each client
-            send_thread = threading.Thread(target=send_messages, args=(client_socket, client_address))
-            send_thread.start()
-
+        client_socket.connect((host, port))
+        print(f"Connected to {host}:{port}")
+        # Start a thread to receive messages from the server
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket, (host, port)))
+        receive_thread.start()
+        # Start a thread to send messages to the server
+        send_thread = threading.Thread(target=send_messages, args=(client_socket, (host, port)))
+        send_thread.start()
     except Exception as e:
-        print(f"Error: {str(e)}")
+        print(f"Error connecting to {host}:{port}: {str(e)}")
+
+def main():
+    while True:
+        print("1. Start as server")
+        print("2. Connect to a server")
+        choice = input("Enter your choice (1/2): ")
+        if choice == '1':
+            host = '0.0.0.0'
+            port = 8888
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                server_socket.bind((host, port))
+                server_socket.listen(5)
+                print("Waiting for connections...")
+                while True:
+                    client_socket, client_address = server_socket.accept()
+                    print(f"Connected to {client_address[0]}:{client_address[1]}")
+                    # Start a thread to receive messages from the client
+                    receive_thread = threading.Thread(target=receive_messages, args=(client_socket, client_address))
+                    receive_thread.start()
+                    # Start a thread to send messages to the client
+                    send_thread = threading.Thread(target=send_messages, args=(client_socket, client_address))
+                    send_thread.start()
+            except Exception as e:
+                print(f"Error: {str(e)}")
+        elif choice == '2':
+            remote_host = input("Enter the remote server's IP address: ")
+            remote_port = int(input("Enter the remote server's port: "))
+            connect_to_server(remote_host, remote_port)
+        else:
+            print("Invalid choice. Please enter '1' or '2'.")
 
 if __name__ == "__main__":
     main()
