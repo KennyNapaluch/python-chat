ff7de682269cde477e422e1a391135aa16bb6221
Kenny Napaluch <121361808+KennyNapaluch@users.noreply.github.com>
2023-09-28 13:09:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,104 @@
 import socket
 import threading
 
+# Create a lock for output synchronization
+output_lock = threading.Lock()
+
+# Define user information
+adrianna_ip = '192.168.4.62'
+kenny_ip = '192.168.4.21'
+my_ip = socket.gethostbyname(socket.gethostname())
+port = 8888
+
+
+def safe_print(message):
+    # Lock to ensure only one thread prints at a time
+    with output_lock:
+        print(message)
+
+
 def receive_messages(client_socket, client_address):
-    print(f"Connected to {client_address[0]}:{client_address[1]}")
+    safe_print(f"Connected to {client_address[0]}:{client_address[1]}")
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
             if not message:
                 break
-            print(f"{client_address[0]}:{client_address[1]} says: {message}")
+            safe_print(f"{client_address[0]}:{client_address[1]} says: {message}")
         except Exception as e:
-            print(f"Error receiving message from {client_address[0]}:{client_address[1]}: {str(e)}")
+            safe_print(f"Error receiving message from {client_address[0]}:{client_address[1]}: {str(e)}")
             client_socket.close()
             break
+
 
 def send_messages(client_socket, client_address):
     while True:
         try:
-            message = input("You: ")  # Input message from the server user
+            message = input()  # Input message from the server user
             client_socket.send(message.encode('utf-8'))
         except Exception as e:
-            print(f"Error sending message to {client_address[0]}:{client_address[1]}: {str(e)}")
+            safe_print(f"Error sending message to {client_address[0]}:{client_address[1]}: {str(e)}")
             client_socket.close()
             break
 
-def connect_to_server(host, port):
+
+def server_thread():
+    host = '0.0.0.0'
+    port = 8888
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server_socket.bind((host, port))
+        server_socket.listen(5)
+        safe_print("Waiting for connections...")
+        while True:
+            client_socket, client_address = server_socket.accept()
+            safe_print(f"Connected to {client_address[0]}:{client_address[1]}")
+            # Start a thread to receive messages from the client
+            receive_thread = threading.Thread(target=receive_messages, args=(client_socket, client_address))
+            receive_thread.start()
+            # Start a thread to send messages to the client
+            send_thread = threading.Thread(target=send_messages, args=(client_socket, client_address))
+            send_thread.start()
+    except Exception as e:
+        safe_print(f"Error: {str(e)}")
+
+
+def client_thread(remote_host, remote_port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        client_socket.connect((host, port))
-        print(f"Connected to {host}:{port}")
+        client_socket.connect((remote_host, remote_port))
+        safe_print(f"Connected to {remote_host}:{remote_port}")
         # Start a thread to receive messages from the server
-        receive_thread = threading.Thread(target=receive_messages, args=(client_socket, (host, port)))
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket, (remote_host, remote_port)))
         receive_thread.start()
         # Start a thread to send messages to the server
-        send_thread = threading.Thread(target=send_messages, args=(client_socket, (host, port)))
+        send_thread = threading.Thread(target=send_messages, args=(client_socket, (remote_host, remote_port)))
         send_thread.start()
     except Exception as e:
-        print(f"Error connecting to {host}:{port}: {str(e)}")
+        safe_print(f"Error connecting to {remote_host}:{remote_port}: {str(e)}")
+
 
 def main():
-    while True:
-        print("1. Start as server")
-        print("2. Connect to a server")
-        choice = input("Enter your choice (1/2): ")
-        if choice == '1':
-            host = '0.0.0.0'
-            port = 8888
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                server_socket.bind((host, port))
-                server_socket.listen(5)
-                print("Waiting for connections...")
-                while True:
-                    client_socket, client_address = server_socket.accept()
-                    print(f"Connected to {client_address[0]}:{client_address[1]}")
-                    # Start a thread to receive messages from the client
-                    receive_thread = threading.Thread(target=receive_messages, args=(client_socket, client_address))
-                    receive_thread.start()
-                    # Start a thread to send messages to the client
-                    send_thread = threading.Thread(target=send_messages, args=(client_socket, client_address))
-                    send_thread.start()
-            except Exception as e:
-                print(f"Error: {str(e)}")
-        elif choice == '2':
-            remote_host = input("Enter the remote server's IP address: ")
-            remote_port = int(input("Enter the remote server's port: "))
-            connect_to_server(remote_host, remote_port)
-        else:
-            print("Invalid choice. Please enter '1' or '2'.")
+    if my_ip == kenny_ip:
+        me = 'Kenny'
+        you = 'Adrianna'
+    elif my_ip == adrianna_ip:
+        me = 'Adrianna'
+        you = 'Kenny'
+    print("1. Start as server")
+    print("2. Connect to a server")
+    choice = input("Enter your choice (1/2): ")
+    if choice == '1':
+        server_thread()
+    elif choice == '2':
+        if me == 'Kenny':
+            client_thread(adrianna_ip, port)
+        elif me == 'Adrianna':
+            client_thread(kenny_ip, port)
+    else:
+        safe_print("Invalid choice. Please enter '1' or '2'.")
+
 
 if __name__ == "__main__":
     main()
