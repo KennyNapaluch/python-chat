295f0316849f81f1ef1f8f65fad36612944c56d6
Kenny Napaluch <121361808+KennyNapaluch@users.noreply.github.com>
2023-09-28 12:14:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import socket
 import threading
 
-
-def receive_messages(client_socket, client_address):
+def handle_client(client_socket, client_address):
     print(f"Connected to {client_address[0]}:{client_address[1]}")
-    while True:
-        try:
+
+    try:
+        while True:
             message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
             print(f"{client_address[0]}:{client_address[1]} says: {message}")
-        except Exception as e:
-            print(f"Error receiving message from {client_address[0]}:{client_address[1]}: {str(e)}")
-            break
 
+    except Exception as e:
+        print(f"Error receiving message from {client_address[0]}:{client_address[1]}: {str(e)}")
 
-def send_messages(client_socket):
-    while True:
-        message = input()
-        client_socket.send(message.encode('utf-8'))
-
+    finally:
+        print(f"Connection to {client_address[0]}:{client_address[1]} closed.")
+        client_socket.close()
 
 def main():
     host = '0.0.0.0'
@@ -27,31 +26,13 @@ def main():
 
     try:
         server_socket.bind((host, port))
-        server_socket.listen(1)
-        print("Waiting for a connection...")
+        server_socket.listen(5)
+        print("Waiting for connections...")
 
-        client_socket, client_address = server_socket.accept()
-
-        receive_thread = threading.Thread(target=receive_messages, args=(client_socket, client_address))
-        send_thread = threading.Thread(target=send_messages, args=(client_socket,))
-
-        receive_thread.start()
-        send_thread.start()
-
-        # Now, we allow the user to initiate a connection to the other peer
         while True:
-            target_port = 8888
-
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            try:
-                client_socket.connect((client_address[0], target_port))
-                print(f"Connected to {client_address[0]}:{target_port}")
-                send_thread = threading.Thread(target=send_messages, args=(client_socket,))
-                send_thread.start()
-                break
-            except Exception as e:
-                print(f"Connection failed: {str(e)}")
+            client_socket, client_address = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
 
     except Exception as e:
         print(f"Error: {str(e)}")
